a193ddcc37780fc9e36ec2259b909c801eed4240
Catherine Seppanen <cseppan@email.unc.edu>
2019-06-30 15:52:07 -0400

    Remove UNC-specific scripts from master branch

--- pg_srgtools/util/create_900915.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO spatial_ref_sys (srid, srtext, proj4text) VALUES
-(900915, 'PROJCS["Polar_Stereographic",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["Sphere_WRF",6370000.0,0.0]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Polar_Stereographic"],PARAMETER
- ["false_easting",0.0],PARAMETER["false_northing",0.0],PARAMETER["central_meridian",-98.0],PARAMETER["scale_factor",1.0],PARAMETER["latitude_of_origin",45.0],UNIT["Meter",1.0"]]', '+proj=stere +lat_ts=45 +lat_0=90 +lon_0=-98 +k_0=1.0 +x_0=98 +y_0=90 +a=6370000 +b=6370000 +units=m +no_defs');
